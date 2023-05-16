aff4cdb78b81e5c5d5b7238c3c27b5c17fe6ce78
Laura Madigan <laura@madigan.org>
2023-05-16 20:37:35 +0100

    added icons

--- app/models/Reading.java
+++ app/models/Reading.java
@@ -78,6 +78,39 @@ public class Reading extends Model {
     return description;
   }
 
+  public String selectIconForConditions() {
+    String icon;
+    switch (this.code) {
+      case 100:
+        icon = "fa-sun"; // Clear
+        break;
+      case 200:
+        icon = "fa-cloud-sun"; // Partial Clouds
+        break;
+      case 300:
+        icon = "fa-cloud"; // Cloudy
+        break;
+      case 400:
+        icon = "fa-cloud-sun-rain"; // Showers
+        break;
+      case 500:
+        icon = "fa-cloud-showers-heavy"; // Heavy Showers
+        break;
+      case 600:
+        icon = "fa-cloud-rain"; // Rain
+        break;
+      case 700:
+        icon = "fa-snowflake"; // Snow
+        break;
+      case 800:
+        icon = "fa-cloud-bolt"; // Thunder
+        break;
+      default:
+        icon = "Unknown"; // Unknown
+    }
+    return icon;
+  }
+
   public int getBeaufortScale() {
     if (this.windSpeed == 1) {
       return 0;
